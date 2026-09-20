5e5ddca81304a80561f15f6550bc0c365a316aa1
mesdaniel1984 <mesdaniel@gmail.com>
2026-09-20 09:23:39 -0300

    test: adiciona usuario sem papel para F4.3

---
+++ phase4/test/bootstrap_sandbox.sql
@@ -0,0 +1,6 @@
+-- Complemento do bootstrap F4.2 para sandbox.
+insert into auth.users(id) values
+('44444444-4444-4444-4444-444444444444');
+
+insert into public.perfis(id,nome,cargo,readonly) values
+('44444444-4444-4444-4444-444444444444','NoRole','Usuário',false);
